e1c9ea65140553a4467575cf94bc1b076e26b6d8
Philippe Kabore <kaborephil@gmail.com>
2021-08-02 18:52:14 +0000

    learning about input statement

---
+++ lab_input/challenge1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+user_name = input("Please enter your user name:")
+day_ofweek = input("what day of the week it is: ")
+print("Hello, " + user_name +"!" + " Happy " + day_ofweek +"!")
+
